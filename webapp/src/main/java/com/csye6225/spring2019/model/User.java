5bfcf01fd882542c222377905ad2965792743d68
Karan Magdani <magdani.k@husky.neu.edu>
2019-01-27 12:52:22 -0500

    Added dependency for persistence, added POJO class for user

--- webapp/src/main/java/com/csye6225/spring2019/model/User.java
+++ webapp/src/main/java/com/csye6225/spring2019/model/User.java
@@ -1,5 +1,47 @@
 package com.csye6225.spring2019.model;
 
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 //    email and password
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
