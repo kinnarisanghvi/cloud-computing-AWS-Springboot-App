8c289cffc99b8ed60ce231848e6d609a07807a06
tJinansi <thakkar.j@husky.neu.edu>
2019-01-31 02:03:41 -0500

    change in validation and gitignore

--- core/src/main/java/com/csye6225/spring2019/controller/UserController.java
+++ core/src/main/java/com/csye6225/spring2019/controller/UserController.java
@@ -6,6 +6,7 @@ import com.csye6225.spring2019.utils.Password;
 import com.csye6225.spring2019.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -40,35 +41,33 @@ public class UserController {
 
         List<User> users = userRepository.findAll();
         for(User user1 : users){
-            if(isValid(user1.getEmailID())){
+
                 if(user.getEmailID().equals(user1.getEmailID())){
                     return "Account already exits";
+                }else if(isValidEmailAddress(user1.getEmailID())){
+//                    boolean valid = EmailValidator.getInstance().isValid(user1.getEmailID());
+//                    if(valid) {
+                        user.setPassword(Password.hashPassword(user1.getPassword()));
+                        userRepository.save(user);
+                        return user.getEmailID().toString() + " " + user.getPassword().toString();
+//                    }else{
+//                        return "INvalid emailId";
+//                    }
                 }else{
-                    user.setPassword(Password.hashPassword(user1.getPassword()));
-                    userRepository.save(user);
-                    return user.getEmailID().toString() + " " + user.getPassword().toString();
+                    return "Invalid Email";
                 }
-            }else{
-                return "Invalid password";
-            }
-
         }
 
         return null;
     }
 
-    public static boolean isValid(String email)
-    {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                "[a-zA-Z0-9_+&*-]+)*@" +
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
-
-        Pattern pat = Pattern.compile(emailRegex);
-        if (email == null)
-            return false;
-        return pat.matcher(email).matches();
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
     }
+
 //    @GetMapping("/notes/{id}")
 //    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
 //        return userRepository.findById(noteId)
