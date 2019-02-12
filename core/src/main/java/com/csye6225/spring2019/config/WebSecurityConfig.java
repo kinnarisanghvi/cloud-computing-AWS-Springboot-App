491116f2257643ac86b1192ce1abb963d764ee79
tJinansi <thakkar.j@husky.neu.edu>
2019-02-11 20:16:41 -0500

    add controller changes

--- core/src/main/java/com/csye6225/spring2019/config/WebSecurityConfig.java
+++ core/src/main/java/com/csye6225/spring2019/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.csye6225.spring2019.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -37,9 +40,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
      @Autowired
      public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
           auth.inMemoryAuthentication().withUser("jinansi").password("password").roles("USER");
+//          auth.userDetailsService()
      }
 
 }
