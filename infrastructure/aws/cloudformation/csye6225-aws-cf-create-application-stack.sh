12b50f691d4d2c6632e05d16f133c8541c354517
kinnarisanghvi <sanghvi.ki@husky.neu.edu>
2019-02-20 01:17:39 -0500

    Modified Application.json and creat_app_stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -5,13 +5,17 @@ set -e
 echo "Please Enter the Application Stack Name: "
 read appstack
 
-echo "please Enter the network stack name: "
-read networkstack
-
 echo "Please enter the key pair name for your stack: "
 read keyname
 
+echo "Please enter the VPCID: "
+read VPCID
 
-aws cloudformation deploy --template ./csye6225-cf-application.json --stack-name "$appstack" --parameter-overrides NetworkStackParameters="$networkstack" KeyPairName="$keyname"
+echo "Please enter the subnetID: "
+read subnetID 
 
+echo "Please enter your AMI ID"
+read AMIID
+
+aws cloudformation deploy --template ./csye6225-cf-application.json --stack-name "$appstack" --parameter-overrides KeyPairName="$keyname" VPCID="$VPCID" subnetID="$subnetID" AMIID="$AMIID"s
 
