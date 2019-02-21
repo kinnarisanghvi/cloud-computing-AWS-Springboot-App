a58b19afcf2b63917005a8716fe348d942f88292
tJinansi <thakkar.j@husky.neu.edu>
2019-02-21 11:26:06 -0800

    remove unnecessary code

--- core/src/main/java/com/csye6225/spring2019/repository/AttachmentRepository.java
+++ core/src/main/java/com/csye6225/spring2019/repository/AttachmentRepository.java
@@ -11,31 +11,31 @@ import java.util.List;
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, String> {
 
-    public default List<Attachment> findBy(String id) {
-        List<Attachment> attachmentList = this.findAll();
-        Attachment foundattachment = null;
-        for (Attachment attachment1 : attachmentList) {
-            System.out.println("attachmentrepo "+ attachment1);
-            if (attachment1.getAttachmentId().equals(id)){
-                foundattachment = attachment1;
-                System.out.println("foundattachment " + foundattachment);
-                break;
-            }
-        }
-
-        Note note = foundattachment.getNote();
-        String note_id = note.getNoteId();
-        List<Attachment> attachments = this.findAll();
-        for (Attachment attachment1 : attachmentList) {
-            System.out.println("attachmentrepo "+ attachment1);
-            if (attachment1.getNote().getNoteId().equals(note_id)){
-                attachments.add(attachment1);
-
-            }
-        }
-
-        return attachments;
-    }
+//    public default List<Attachment> findBy(String id) {
+//        List<Attachment> attachmentList = this.findAll();
+//        Attachment foundattachment = null;
+//        for (Attachment attachment1 : attachmentList) {
+//            System.out.println("attachmentrepo "+ attachment1);
+//            if (attachment1.getAttachmentId().equals(id)){
+//                foundattachment = attachment1;
+//                System.out.println("foundattachment " + foundattachment);
+//                break;
+//            }
+//        }
+//
+//        Note note = foundattachment.getNote();
+//        String note_id = note.getNoteId();
+//        List<Attachment> attachments = this.findAll();
+//        for (Attachment attachment1 : attachmentList) {
+//            System.out.println("attachmentrepo "+ attachment1);
+//            if (attachment1.getNote().getNoteId().equals(note_id)){
+//                attachments.add(attachment1);
+//
+//            }
+//        }
+//
+//        return attachments;
+//    }
 
 
 }
