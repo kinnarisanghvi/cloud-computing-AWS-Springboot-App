872da0cb17aec33ef7d1315d077647a195afb533
vigneshraghuraman1991 <raghuraman.v@husky.neu.edu>
2019-02-21 11:22:09 -0800

    PUT logic change

--- core/src/main/java/com/csye6225/spring2019/controller/AttachmentController.java
+++ core/src/main/java/com/csye6225/spring2019/controller/AttachmentController.java
@@ -142,7 +142,7 @@ public class AttachmentController {
             String attachmentID = attachment.getAttachmentId();
             String url = attachment.getUrl();
             this.amazonClient.deleteFileFromS3Bucket(url);
-            attachmentRepository.delete(attachment);
+
 
 
             String url1 = this.amazonClient.uploadFile(file);
@@ -152,11 +152,13 @@ public class AttachmentController {
                 return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
             }
 
-            Attachment attachment1 = new Attachment();
-            attachment.setAttachmentId(attachmentID);
-            attachment.setUrl(url1);
-            //note.getAttachmentList().add(attachment);
-            attachmentRepository.save(attachment1);
+             attachment.setUrl(url1);
+             attachment.getNote().setNoteId(idNotes);
+
+
+
+
+            attachmentRepository.save(attachment);
 
 //            List<JSONObject> entities = new ArrayList<JSONObject>();
 //            JSONObject entity = new JSONObject();
