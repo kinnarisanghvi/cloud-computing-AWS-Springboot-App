37572fa92ed704144fb11cd25b1b1e756bbd2b85
karanmagdani <magdani.k@husky.neu.edu>
2019-03-13 07:35:03 -0400

    Made changes to main method to deploy war on tomcat

--- core/src/main/java/com/csye6225/spring2019/NotesApplication.java
+++ core/src/main/java/com/csye6225/spring2019/NotesApplication.java
@@ -3,14 +3,21 @@ package com.csye6225.spring2019;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class NotesApplication {
+public class NotesApplication extends SpringBootServletInitializer {
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NotesApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
 	}
+
+
 }
