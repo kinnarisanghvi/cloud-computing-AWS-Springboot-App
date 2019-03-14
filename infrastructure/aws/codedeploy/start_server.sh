9aa067acb67474a4fa0d45c4ab4407343ee77b48
karanmagdani <magdani.k@husky.neu.edu>
2019-03-14 07:57:45 -0400

    added env variable in appspec

--- infrastructure/aws/codedeploy/start_server.sh
+++ infrastructure/aws/codedeploy/start_server.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+echo "bucketName=csye6225-spring2019-magdanik.me.csye6225.com" >> ~/.bashrc
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo systemctl start tomcat
