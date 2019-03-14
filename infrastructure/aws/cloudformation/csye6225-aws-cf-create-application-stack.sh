5e549cb41ecea98471e83c712b47986b0f84f90a
tJinansi <thakkar.j@husky.neu.edu>
2019-03-14 11:15:19 -0700

    change in permissions

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -8,8 +8,8 @@ read appstack
 #echo "Please enter the key pair name for your stack:"
 #read keyname
 
-#echo "Please enter your AMI ID"
-#read AMIID
+echo "Please enter your bucketname"
+read bucketName
 
 
 export VPCID=$(aws ec2 describe-vpcs --filters "Name=cidr,Values=10.0.0.0/16" --query "Vpcs[*].[CidrBlock, VpcId][-1]" --output text|grep 10.0.0.0/16|awk '{print $2}')
@@ -36,7 +36,7 @@ echo "Creating Application stack"
         echo "AMI ID:${AMIID}"
 	while [ ${AMIID} != "" ]; 
         do
-        STACK_STATUS=$(aws cloudformation deploy --template ./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --stack-name "$appstack" --parameter-overrides KeyPairName="$keypair" VPCID="$VPCID" subnetID1="$subnetID1" subnetID2="$subnetID2" subnetID3="$subnetID3" AMIID="$AMIID")
+        STACK_STATUS=$(aws cloudformation deploy --template ./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --stack-name "$appstack" --parameter-overrides KeyPairName="$keypair" VPCID="$VPCID" subnetID1="$subnetID1" subnetID2="$subnetID2" subnetID3="$subnetID3" AMIID="$AMIID" bucketName="$bucketName")
         echo $STACK_STATUS
         done
         echo "Stack ${appstack} Created successfully!"
