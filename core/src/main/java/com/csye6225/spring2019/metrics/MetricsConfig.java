1ec4eaad839a745e7b852b4d035be5c548849a6e
tJinansi <thakkar.j@husky.neu.edu>
2019-03-20 10:40:47 -0700

    changes in appspec for cloud watch

--- core/src/main/java/com/csye6225/spring2019/metrics/MetricsConfig.java
+++ core/src/main/java/com/csye6225/spring2019/metrics/MetricsConfig.java
@@ -13,7 +13,7 @@ public class MetricsConfig {
     public StatsDClient statsDClient(
             @Value("${metrics.statsd.host:localhost}") String host,
             @Value("${metrics.statsd.port:8125}") int port,
-            @Value("${metrics.prefix:example.app}") String prefix
+            @Value("${metrics.prefix:notes-application.app}") String prefix
     ) {
         return new NonBlockingStatsDClient(prefix, host, port);
     }
