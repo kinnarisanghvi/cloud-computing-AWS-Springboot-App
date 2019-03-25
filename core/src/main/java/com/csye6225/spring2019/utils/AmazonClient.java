62e63f84edbfdd12d58c7597d75dcf5a16decde3
karanmagdani <magdani.k@husky.neu.edu>
2019-03-25 19:54:08 -0400

    Added SNS Topic code in webapp

--- core/src/main/java/com/csye6225/spring2019/utils/AmazonClient.java
+++ core/src/main/java/com/csye6225/spring2019/utils/AmazonClient.java
@@ -1,12 +1,14 @@
 package com.csye6225.spring2019.utils;
 
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -24,7 +26,9 @@ import java.util.Date;
 public class AmazonClient {
 
     private AmazonS3 s3client;
-    
+
+    @Autowired
+    SNSPublisherService publisherService;
 
     @Value("${aws.region}")
     private String awsRegion;
@@ -76,6 +80,15 @@ public class AmazonClient {
         String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
         s3client.deleteObject(new DeleteObjectRequest(awsS3AudioBucket , fileName));
         return "Successfully deleted";
+    }
+
+    public String publishSNSTopic() {
+        try {
+            this.publisherService.publish("{\"mobileno\": \"+6176376750\", \"message\": \"Hello Karan! How have you been doing? Hope you are doing great. Good bye! Also fuck you\"}", SNSPublisherService.TOPIC_RESETPASSWORD);
+        } catch (AmazonServiceException e) {
+            e.printStackTrace();
+        }
+        return "Topic Published";
     }
 
     @Bean
---
+++ core/src/main/java/com/csye6225/spring2019/utils/AmazonSNSPublisherService.java
@@ -0,0 +1,56 @@
+package com.csye6225.spring2019.utils;
+
+import com.amazonaws.AmazonServiceException;
+import com.csye6225.spring2019.utils.SNSPublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+
+@Service
+public class AmazonSNSPublisherService implements SNSPublisherService {
+
+    private AmazonSNS amazonSNS;
+    private String snsTopicResetPasswordARN;
+
+    @Autowired
+    public AmazonSNSPublisherService(BasicSessionCredentials sessionCredentials, String snsTopicResetPasswordARN) {
+        this.amazonSNS = AmazonSNSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(sessionCredentials)).build();
+        this.snsTopicResetPasswordARN = snsTopicResetPasswordARN;
+    }
+
+    @Override
+    public void publish(String message, String topic) throws AmazonServiceException {
+        //
+        // Get Appropriate Topic ARN
+        //
+        String snsTopic = getTopicARN(topic);
+        //
+        // Create Publish Message Request with TopicARN and Message
+        //
+        PublishRequest publishRequest = new PublishRequest(snsTopic, message);
+        //
+        // Publish the message
+        //
+        PublishResult publishResult = this.amazonSNS.publish(publishRequest);
+        //
+        // Evaluate the result: Print MessageId of message published to SNS topic
+        //
+        System.out.println("MessageId - " + publishResult.getMessageId());
+
+    }
+
+    private String getTopicARN(String topic) throws AmazonServiceException {
+        switch(topic) {
+            case TOPIC_RESETPASSWORD:
+                return this.snsTopicResetPasswordARN;
+            default:
+                throw new AmazonServiceException("No matching topic supported!");
+        }
+    }
+}
