9696f445bd6b8049a4611fc84729663176ba7a7e
tJinansi <thakkar.j@husky.neu.edu>
2019-04-02 19:43:05 -0700

    remove typos

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -44,7 +44,7 @@ certificatearn=$(aws acm list-certificates --query "CertificateSummaryList[0].Ce
 echo "Certificate Arn is $certificatearn"
 
 
-aws cloudformation deploy --template ./csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_NAMED_IAM --stack-name "$autoscalestack" --parameter-overrides KeyPairName="$keypair" BucketName="$bucketName" AccountId="$accountid" EC2ImageId="$ec2ImageId" EC2InstanceType="$ec2InstanceType" HostedZoneId="$hostedzoneid" HostedZoneName="$hostedzonename" CertificateArn="$certificatearn" AssociatePublicAddress="$associatePublicAddress" VPCID="$VPCID" subnetID1="$subnetID1" subnetID2="$subnetID2" subnetID3="$subnetID3" AMIID="$AMIID" CertificateArn="$certificatearn"
+aws cloudformation deploy --template ./csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_NAMED_IAM --stack-name "$autoscalestack" --parameter-overrides KeyPairName="$keypair" bucketName="$bucketName" AccountId="$accountid" EC2ImageId="$ec2ImageId" EC2InstanceType="$ec2InstanceType" HostedZoneId="$hostedzoneid" HostedZoneName="$hostedzonename" CertificateArn="$certificatearn" AssociatePublicAddress="$associatePublicAddress" VPCID="$VPCID" subnetID1="$subnetID1" subnetID2="$subnetID2" subnetID3="$subnetID3" AMIID="$AMIID" CertificateArn="$certificatearn"
 
 
 
