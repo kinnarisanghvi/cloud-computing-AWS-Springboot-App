a620d46ba0b4d51fff15d78d9e2d7080f1824f75
jinansithakkar <43270166+tJinansi@users.noreply.github.com>
2019-04-05 21:50:12 -0400

    Update removeRoot.sh

--- infrastructure/aws/codedeploy/removeRoot.sh
+++ infrastructure/aws/codedeploy/removeRoot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-set -e
-while[ ! -f /opt/tomcat/samplefile ]
-do
-    echo "waiting for ec2 intsance.."
-    sleep 2
-done
 echo "ec2 instance is running deploying code"
 sudo systemctl stop tomcat
 sudo rm -rf /opt/tomcat/apache-tomcat-9.0.17/webapps/ROOT
