209f534e5d1e6e3c942183d2710e2ac3b750feba
karanmagdani <magdani.k@husky.neu.edu>
2019-04-08 18:31:15 -0400

    added traffic control test

--- infrastructure/aws/codedeploy/stop_server.sh
+++ infrastructure/aws/codedeploy/stop_server.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json -s &
