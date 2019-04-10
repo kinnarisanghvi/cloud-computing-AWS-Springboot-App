7505f0cac854f2e7ca4a46315f2413a5515d2b67
kinnari sanghvi <35201721+kinnarisanghvi@users.noreply.github.com>
2019-04-10 10:01:21 -0400

    Update stop_server.sh

--- infrastructure/aws/codedeploy/stop_server.sh
+++ infrastructure/aws/codedeploy/stop_server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-set -e
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json -s &
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json -s
